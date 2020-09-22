fe0cae199365676874af9517565e587063b09a83
MynorHerrera <mynorcampos2008@hotmail.com>
2020-09-21 20:18:04 -0600

    editando ruta

--- api/router/indexRouter.js
+++ api/router/indexRouter.js
@@ -2,10 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-
 app.use("/api", require("./usuarioRouter"));
 app.use("/api", require("./productoRouter"));
 app.use("/api", require("./clienteRouter"));
